782b29e8d0191dc87bbb67779326fc8706a0c8ec
Phap <Phap@DESKTOP-J1MK4JS>
2018-08-06 09:32:57 +0700

    Thirst Edittion

--- src/java/controller/StudentController.java
+++ src/java/controller/StudentController.java
@@ -5,6 +5,7 @@
  */
 package controller;
 
+import business.Student;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,6 +22,14 @@ public class StudentController extends HttpServlet{
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException{
-        
+        Student st= new Student(1, "Huynh Phuoc Phap");
+        request.setAttribute("student", st);
+        String url = "/thanks.jsp";
+        getServletContext().getRequestDispatcher(url).forward(request, response);
+    }
+     @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException{
+         doPost(request, response);
     }
 }
---
+++ web/thanks.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : thanks
+    Created on : Aug 6, 2018, 9:31:17 AM
+    Author     : Phap
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello Student!</h1>
+        <p>Name: ${student.studentName}</p>
+    </body>
+</html>
